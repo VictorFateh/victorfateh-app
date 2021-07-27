a904e3fcd4ce79725e299a75a13c6374d503eae1
Victor Fateh <victorfateh@yahoo.com>
2021-07-26 21:55:15 -0700

    feat(navbar): navbar to component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app floating class="hidden-md-and-up">
-      <v-list class="text-center">
-        <v-list-item link @click="drawer = false">
-          <v-list-item-title>
-            <v-icon>mdi-close</v-icon>
-          </v-list-item-title>
-        </v-list-item>
-        <v-list-item link to="/">
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item>
-        <v-list-item v-for="m in menu" :key="m.title" link :to="m.to" exact>
-          <v-list-item-content>
-            <v-list-item-title>{{ m.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app color="white" flat>
-      <v-container class="py-0 fill-height">
-        <v-avatar class="mr-10" color="grey darken-1" size="32" />
-
-        <v-spacer />
-
-        <v-btn v-for="link in menu" :key="link.title" text :to="link.to">
-          {{ link.title }}
-        </v-btn>
-      </v-container>
-    </v-app-bar>
+    <nav-bar />
     <v-main>
       <v-container fluid>
         <router-view />
@@ -41,11 +13,9 @@
 import { reactive, ref } from "@vue/composition-api";
 
 export default {
-  setup(_props, context) {
-    const vuetify = context.root.$vuetify;
-
+  components: { NavBar: () => import("./components/core/NavBar.vue") },
+  setup(_props) {
     // UI
-    const dark = ref(vuetify.theme.dark);
     const drawer = ref();
 
     // Data
@@ -65,12 +35,6 @@ export default {
     ]);
     const homeTitle = ref("Victor Fateh");
 
-    // events handler
-    function toggleTheme() {
-      dark.value = !dark.value;
-      vuetify.theme.dark = dark.value;
-    }
-
     return {
       // UI
       drawer,
@@ -79,8 +43,6 @@ export default {
       menu,
       homeTitle,
 
-      // Functions
-      toggleTheme,
     };
   },
 };
---
+++ src/components/core/NavBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app floating class="hidden-md-and-up">
+      <v-list class="text-center">
+        <v-list-item link @click="drawer = false">
+          <v-list-item-title>
+            <v-icon>mdi-close</v-icon>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/">
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item v-for="m in menu" :key="m.title" link :to="m.to" exact>
+          <v-list-item-content>
+            <v-list-item-title>{{ m.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="white" flat>
+      <v-container class="py-0 fill-height">
+        <v-avatar class="mr-10" color="grey darken-1" size="32" />
+
+        <v-spacer />
+
+        <v-btn v-for="link in menu" :key="link.title" text :to="link.to">
+          {{ link.title }}
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from "@vue/composition-api";
+
+export default {
+  name: "Navbar",
+
+  setup() {
+    // UI
+    const drawer = ref();
+
+    // Data
+    const menu = reactive([
+      {
+        title: "About",
+        to: "/about",
+      },
+      {
+        title: "Experience",
+        to: "/experience",
+      },
+      {
+        title: "Contact",
+        to: "/contact",
+      },
+    ]);
+
+    return { drawer, menu };
+  },
+};
+</script>
+
+<style>
+
+</style>

ccbb233278c76328bbb3c4f735a20f64e429c959
Victor Fateh <victorfateh@yahoo.com>
2021-10-10 15:53:31 -0700

    feat: add images

---
+++ src/views/Connect.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "connect",
+  metaInfo: { title: "Connect" }
+});
+</script>
